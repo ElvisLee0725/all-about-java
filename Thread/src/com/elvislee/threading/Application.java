4d823b4c9b574182c9ae73b43ba5515b1a942e15
ElvisLee0725 <elvislee0725@gmail.com>
2021-09-17 17:15:50 -0700

    Different ways to instantiate a thread

--- Thread/src/com/elvislee/threading/Application.java
+++ Thread/src/com/elvislee/threading/Application.java
@@ -2,21 +2,71 @@ package com.elvislee.threading;
 
 public class Application {
     public static void main(String[] args) {
-        Task thread1 = new Task();
-        thread1.start();
 
-        Task thread2 = new Task();
-        thread2.start();
+        // Create a thread, method 1:
+        System.out.println("Start thread 1: ");
+        Thread t1 = new Thread(new Task("Thread-a"));
+        t1.start();
 
-        System.out.println("Hello world");
+        // Create a thread, method 2:
+        System.out.println("Start thread 2: ");
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               // Thread.currentThread().setName(this.name);
+
+                for(int i = 0; i < 100; i++) {
+                    System.out.println("Number: " + i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t2.start();
     }
 }
 
-class Task extends Thread {
+class Task implements Runnable {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
     @Override
     public void run() {
-        for(int i = 0; i < 1000; i++) {
-            System.out.println("Number: " + i);
+        Thread.currentThread().setName(this.name);
+
+        for(int i = 0; i < 100; i++) {
+            System.out.println("Number: " + i + " - " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
+
+//class Task extends Thread {
+//    String name;
+//
+//    public Task(String name) {
+//        this.name = name;
+//    }
+//    @Override
+//    public void run() {
+//        Thread.currentThread().setName(this.name);
+//
+//        for(int i = 0; i < 100; i++) {
+//            System.out.println("Number: " + i + " - " + Thread.currentThread().getName());
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
